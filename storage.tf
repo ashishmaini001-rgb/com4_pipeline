31282324b0476a7094833a18ec43ced7701e2e7c
Ashish <ashish.maini00111@gmail.com>
2026-05-30 19:11:48 +0530

    storage account code & file added

---
+++ storage.tf
@@ -0,0 +1,8 @@
+resource "azurerm_storage_account" "stg1" {
+  name = "newgitpractice11"
+  location = "West US"
+  resource_group_name = azurerm_resource_group.block1.name
+  account_tier = "Standard"
+  account_replication_type = "GRS"
+
+}
