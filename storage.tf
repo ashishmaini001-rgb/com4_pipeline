ad218223c8a56820f8c633c664bf83036a443b1a
Ashish <ashish.maini00111@gmail.com>
2026-05-30 20:34:06 +0530

    location added east us

--- storage.tf
+++ storage.tf
@@ -1,6 +1,6 @@
 resource "azurerm_storage_account" "stg1" {
   name = "newgitpractice11"
-  location = "West US"
+  location = "East US"
   resource_group_name = azurerm_resource_group.block1.name
   account_tier = "Standard"
   account_replication_type = "LRS"
