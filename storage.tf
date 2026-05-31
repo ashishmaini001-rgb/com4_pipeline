5e5a6f251c27d1cde32895710141965d62111219
Ashish Maini <ashish.maini001@gmail.com>
2026-05-31 16:20:14 +0530

    NEW_STORAGE_ACCOUNT_Codeadded in storage.tf

--- storage.tf
+++ storage.tf
@@ -6,3 +6,12 @@ resource "azurerm_storage_account" "stg1" {
   account_replication_type = "LRS"
 
 }
+
+resource "azurerm_storage_account" "stg2" {
+  name = "22newgitpractice11"
+  location = "East US"
+  resource_group_name = azurerm_resource_group.block1.name
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+
+}
