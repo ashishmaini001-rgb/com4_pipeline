7697ccff25c8a84ef36ac78cd9c7606a971181be
Ashish <ashish.maini00111@gmail.com>
2026-05-31 16:51:14 +0530

    77777 storage account added

--- storage.tf
+++ storage.tf
@@ -24,3 +24,13 @@ resource "azurerm_storage_account" "stg33" {
   account_replication_type = "LRS"
 
 }
+
+resource "azurerm_storage_account" "stg333" {
+  name = "777777aaaa"
+  location = "East US"
+  resource_group_name = azurerm_resource_group.block1.name
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+
+}
+
