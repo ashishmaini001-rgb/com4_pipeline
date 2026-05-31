7ce1a8d6856a6610a9cd3a92c8ba2958c79c344e
Ashish Maini <ashish.maini001@gmail.com>
2026-05-31 16:26:28 +0530

    Update55555 storage.tf

--- storage.tf
+++ storage.tf
@@ -15,3 +15,12 @@ resource "azurerm_storage_account" "stg2" {
   account_replication_type = "LRS"
 
 }
+
+resource "azurerm_storage_account" "stg33" {
+  name = "555555555aaaa"
+  location = "East US"
+  resource_group_name = azurerm_resource_group.block1.name
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+
+}
